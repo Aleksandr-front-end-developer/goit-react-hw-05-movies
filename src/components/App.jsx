eacb80364e13ba1bde2c45e51a4b94dccaf7364f
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2024-02-17 17:38:15 +0200

    files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,14 +14,14 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/goit-react-hw-05-movies/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />}></Route>
           </Route>
-          <Route path="/goit-react-hw-05-movies/*" element={<Home />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </>
